bb064bca204f1e46f8cedee747eb1ebd5230776d
GameBear64 <vlpetrov02@gmail.com>
2022-07-09 15:10:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Icon } from '@mui/material';
+import styled from 'styled-components';
+
+function App() {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #00ff00;
+  `;
+  const StyledButton = styled(Button)`
+    && {
+      background-color: #006600;
+      border-radius: 0;
+    }
+  `;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Title>Hello World!</Title>
+        <p>
+          Edit <code>src/App.js</code> and save to reload. <br /> Test file, just making sure everything works
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        <br />
+        <Button variant="contained">
+          <Icon>favorite</Icon> Test button - very cool
+        </Button>
+        <br />
+        <StyledButton variant="contained">
+          <Icon>star</Icon> Styled button - even cooler
+        </StyledButton>
+      </header>
+    </div>
+  );
+}
+
+export default App;
