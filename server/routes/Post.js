cb77c20f4b5dfd15052109037e47714e1eb4d695
GameBear64 <vlpetrov02@gmail.com>
2022-08-09 08:26:16 +0300

    brh

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -27,6 +27,7 @@ router.route('/page/:page').get(async (req, res) => {
   .skip(req.params.page * 10)
   .limit(10)
   .populate('images')
+  // .populate('comments')
   .populate('author', 'nickname firstName lastName profilePicture');
 
   res.status(200).send({ posts, count });
@@ -52,6 +53,14 @@ router
   .get(async (req, res) => {
     let post = await PostModel.findOne({ _id: ObjectId(req.params.id) })
       .populate('images')
+      .populate({
+        path: 'comments',
+        populate: {
+          path: 'author',
+          select: ['nickname', 'firstName', 'lastName', 'profilePicture'],
+          // options: { $sort: [[{ $project: { $subtract: ['upVotes', 'downVotes'] } }, -1], { createdAt: -1 }] },
+        },
+      })
       .populate('author', 'nickname firstName lastName profilePicture');
 
     res.status(200).send(post);
@@ -83,8 +92,6 @@ router
   });
 
 router
-=======
->>>>>>> origin/master
   .route('/like/:id')
   .patch(async (req, res) => {
     let post = await PostModel.findOne({ _id: ObjectId(req.params.id) });
@@ -120,45 +127,11 @@ router.route('/byuser/:authorId/:page').get(async (req, res) => {
     .skip(req.params.page * 10)
     .limit(10)
     .populate('images')
+    // .populate('comments')
     .populate('author', 'nickname firstName lastName profilePicture');
 
   res.status(200).send({ posts, count });
 });
-
-router
-  .route('/:id')
-  .get(async (req, res) => {
-    let post = await PostModel.findOne({ _id: ObjectId(req.params.id) })
-      .populate('images')
-      .populate('author', 'nickname firstName lastName profilePicture');
-
-    res.status(200).send(post);
-  })
-  .patch(async (req, res) => {
-    let id = req.body._id;
-    try {
-      let response = filterEditedResponse(req.body);
-      let imageIds = await uploadImages(response.newImages, req);
-      delete response.newImages;
-
-      response.images = response.images.concat(imageIds);
-      await PostModel.updateOne({ _id: ObjectId(id) }, response);
-
-      return res.status(200).send({ message: 'Entry patched' });
-    } catch (err) {
-      return res.status(406).send({ message: 'Error while creating post', error: err });
-    }
-  })
-  .delete(async (req, res) => {
-    let post = await PostModel.findOne({ _id: ObjectId(req.params.id) });
-
-    await post.delete();
-
-    res.status(200).send({ message: 'Entry deleted' });
-  })
-  .all((req, res) => {
-    res.status(405).send({ message: 'Use another method' });
-  });
 
 async function uploadImages(images, req) {
   if (!images) return;
